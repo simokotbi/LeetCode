6205af83ba7fae815a9baeb9de6ca270697541a1
Kotbi Mohamed <simojetta2@gmail.com>
2021-09-18 12:42:03 -0700

    time complexity must be considerated

--- c++/Count_Good_Meals.cpp
+++ c++/Count_Good_Meals.cpp
@@ -13,20 +13,22 @@ public:
         for(int i=0;i<deliciousness.size();i++){
             for(int j=i+1;j<deliciousness.size();j++){
          number=deliciousness[i]+deliciousness[j];
+         if(number!=1){
         while(number % 2==0 && number!=1){
              number/=2;
              if(number==1){
                  count++;
                  
-             }
-        }}
+             }}
+        }else{count++;}
+        }
          }return count;
     }
 
 };
 int main(){
 Solution s;
- vector<int> vect = {149,107,1,63,0,1,6867,1325,5611,2581,39,89,46,18,12,20,22,234};
+ vector<int> vect = {0,1,18,12,0,1,20,22};
 s.countPairs(vect);
 
     return 0;
