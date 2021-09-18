6ca93579dcccd5eb835056b54053b947293070dc
Kotbi Mohamed <simojetta2@gmail.com>
2021-09-18 11:03:06 -0700

    count good meals

---
+++ c++/Count_Good_Meals.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+  int countPairs(vector<int>& deliciousness) {
+        //find if it is a power of 2:
+        for(int i=0;i<deliciousness.size();i++){
+            
+        int number;
+        while(number%2=0&&number!=1){
+             number/=2;
+             if(number==1){
+                 return 
+             }
+        }
+         }
+    }
+
+};
+int main(){
+Solution s;
+
+
+    return 0;
+}
