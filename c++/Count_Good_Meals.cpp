c1d6a66c70df159696534e97f3ffed39fe96f7b7
Kotbi Mohamed <simojetta2@gmail.com>
2021-09-19 12:39:46 -0700

    stil time limit exceded

--- c++/Count_Good_Meals.cpp
+++ c++/Count_Good_Meals.cpp
@@ -6,23 +6,28 @@ using namespace std;
 
 class Solution {
 public:
-  int countPairs(vector<int>& deliciousness) {
-        //find if it is a power of 2:
+    int countPairs(vector<int>& deliciousness) {
+      //find if it is a power of 2:
         int count=0;
         int number;
-        for(int i=0;i<deliciousness.size();i++){
-            for(int j=i+1;j<deliciousness.size();j++){
-         number=deliciousness[i]+deliciousness[j];
-         if(number!=1){
-        while(number % 2==0 && number!=1){
+        int j=deliciousness.size()-1;
+        int i=0;
+          while (i<j)
+          {
+             number=deliciousness[i]+deliciousness[j];
+                if(number==1){count++;}
+         
+        while(number % 2==0&&number!=0&&i<j){
              number/=2;
              if(number==1){
                  count++;
-                 
-             }}
-        }else{count++;}
-        }
-         }return count;
+                
+             }
+         }
+         if(i==j-1){i=0;j--;}else{i++;}
+          }
+           
+         return count;
     }
 
 };
