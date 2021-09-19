1c43125fb362d3a0239a83ce09cae1a979e91b0f
Kotbi Mohamed <simojetta2@gmail.com>
2021-09-19 11:17:42 -0700

    remove duplicates update

--- c++/Remove-duplicates.cpp
+++ c++/Remove-duplicates.cpp
@@ -11,7 +11,7 @@ public:
   int n=nums.size();
 int count=1;
  for(int i=1;i<n;i++){
-          if(nums[i-1]!=nums[i]){
+          if(nums[i-1]!=nums[i]){ 
             nums[count]=nums[i];
               count++;
           }
